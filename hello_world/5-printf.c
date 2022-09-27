daf2ac11a81584538a1587f9c657e891bfaec566
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-27 08:10:44 -0700

    adds C program 5-printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - Prints "with proper grammar, but the outcome is a piece of art,\n"
+  *
+  * Description: main uses printf function to print a string
+  * Return: A string of words and a newline
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
