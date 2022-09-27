63303dadf27b0426b033e16b3e863226fb7d5fe4
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-27 08:02:05 -0700

    adds description to 4-puts.c main function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+  * main - Prints "Programming is like building a multilingual puzzle
+  *
+  * Description: main uses puts function to print a string
+  * Return: A string of words
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
