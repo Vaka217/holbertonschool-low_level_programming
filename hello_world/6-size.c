069f1ade7d242920feaa654ac47f1c1202ca949e
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-27 08:41:42 -0700

    adds C file 6-size.c and C programs size32 and size 64

---
+++ hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main -  Prints the size of various types on the computer it is compiled and run on.
+  *
+  * Description: printf() prints a message with the size of each type and each sizeof() gets the value of the size of each type.
+  * Return: A message which each line have its own message telling which type is and its size value in byte(s).
+  */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\nSize of an int: %ld byte(s)\nSize of a long int: %ld byte(s)\nSize of a long long int: %ld byte(s)\nSize of a float: %ld byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+	return (0);
+}
