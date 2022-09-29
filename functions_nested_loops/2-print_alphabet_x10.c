3b7416e788fe73c241fc2c185fa048e87a44fbba
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-29 06:46:53 -0700

    modifies main.h with new prototype function and adds C program 2-print_alphabet_x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "_print_alphabet_x10.c"
+
+/**
+   * main - check the code.
+    *
+     * Return: Always 0.
+      */
+int main(void)
+{
+	    print_alphabet_x10();
+	        return (0);
+}
+
---
+++ functions_nested_loops/_print_alphabet_x10.c
@@ -0,0 +1,16 @@
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+	return;
+}
