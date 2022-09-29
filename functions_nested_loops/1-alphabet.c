f82b1896aa1c3b2165d7c8abff1edb881680c68f
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-29 06:01:35 -0700

    Adds C program 1-alphabet and changes prototype functions inside main.h>

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Description: putchar prints the value of the char variable c, c initializes
+ * with 'a' value and the for statement iterates until c ends with 'z' value
+ * and uses putchar with each value of c.
+ * Return: The alphabet in lowercase.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
