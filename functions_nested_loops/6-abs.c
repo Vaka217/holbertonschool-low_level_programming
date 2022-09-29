c00ee1a399186c92b86dd0beb59c134689888a33
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-29 09:25:55 -0700

    adds a prototype function to main.h and C program 6-abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include "_abs.c"
+/**
+ * abs - Computes the absolute value of an integer
+ *
+ * Description: Computes the absolute value of an integer using _abs.
+ * _abs checks if x number is negative or positive, if its positive prints x,
+ * if not, prints -x.
+ * Return: Always 0.
+ */
+int abs(void)
+{
+	int r;
+
+	r = _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	return (0);
+}
