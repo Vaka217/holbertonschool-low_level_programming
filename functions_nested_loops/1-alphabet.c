085db0fd1e5eb00a7a137bd743278646a6585b62
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-29 06:22:12 -0700

    hot fix

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
     *
      * Return: Always 0.
       */
-int main(void)
+int alfredo(void)
 {
 	print_alphabet();
 	return (0);
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 #include <unistd.h>
+#include <_putchar.c>
+#include <_alphabet.c>
 int _putchar(char c);
 void print_alphabet(void);
