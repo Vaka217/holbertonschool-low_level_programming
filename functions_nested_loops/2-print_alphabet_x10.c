86d2c2029324d3551fdb958da6235df2bd806048
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-29 06:52:03 -0700

    changes function name and betty style

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,17 @@
 #include "main.h"
 #include "_print_alphabet_x10.c"
-
 /**
-   * main - check the code.
-    *
-     * Return: Always 0.
-      */
-int main(void)
+ * abcx10 - Prints the alphabet in lowercase
+ *
+ * Description: putchar prints the value of the char variable c, c initializes
+ * with 'a' value and the for statement iterates until c ends with 'z' value
+ * and uses putchar with each value of c. Then a while iterates over
+ * to print the alphabet 10 times.
+ * Return: Always 0.
+ */
+int abcx10(void)
 {
-	    print_alphabet_x10();
-	        return (0);
+	print_alphabet_x10();
+	return (0);
 }
 
