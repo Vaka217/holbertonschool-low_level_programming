e9bbaa59e1da0eb8cded772b96cac51f29cf0bf8
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-30 05:57:37 -0700

    adds prototype function to main.h and C program 9-times_table

--- functions_nested_loops/_times_table.c
+++ functions_nested_loops/_times_table.c
@@ -2,34 +2,39 @@ void times_table(void)
 {
 	int f = 0;
 	int c = 0;
-	int i = 0;
-
 	for (f = 0; f < 10; f++)
 	{
 		for (c = 0; c < 9; c++)
 		{
-			if (c * i >= 10)
+			if ((f * c < 10) && (f * (c + 1) >= 10))
 			{
-				_putchar((c * i) / 10 + '0');
-				_putchar((c * i) % 10 + '0');
+				_putchar (f * c + '0');
+				_putchar (',');
+				_putchar (' ');
+			}
+			else if (f * c >= 10)
+			{
+				_putchar ((f * c) / 10 + '0');
+				_putchar ((f * c) % 10 + '0');
+				_putchar (',');
+				_putchar (' ');
 			}
 			else
 			{
-				_putchar(c * i + '0');
+			_putchar (f * c + '0');
+			_putchar (',');
+			_putchar (' ');
+			_putchar (' ');
 			}
-			_putchar(',');
-			_putchar(' ');
 		}
-		if (c * i >= 10)
-		{
-			_putchar((c * i) / 10 + '0');
-			_putchar((c * i) % 10 + '0');
-		}
+		if (f * c < 10)
+			_putchar (f * c + '0');
 		else
 		{
-			_putchar(c * i + '0');
+			_putchar ((f * c) / 10 + '0');
+			_putchar ((f * c) % 10 + '0');
 		}
-		i++;
-		_putchar('\n');
+		_putchar ('\n');
 	}
+	return;
 }
