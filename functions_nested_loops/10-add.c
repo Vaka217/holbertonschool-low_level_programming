b41db2d81e5319967d46f7c668191651c26df220
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-30 06:35:30 -0700

    adds prototype function to main.h and C program 10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include "_add.c"
+
+/**
+   * add - check the code
+    *
+     * Return: Always 0.
+      */
+int add(void)
+{
+	    int n;
+
+	        n = add(89, 9);
+		    printf("%d\n", n);
+		        return (0);
+}
