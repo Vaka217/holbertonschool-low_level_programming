2f9982475564cdeae95b30c7efa268e162d94bb4
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-30 06:01:37 -0700

    fixes betty style

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,10 +1,13 @@
 #include "main.h"
 #include "_times_table.c"
 /**
-   * main - check the code
-    *
-     * Return: Always 0.
-      */
+ * timetable - prints the 9 times table, starting with 0.
+ *
+ * Description: iterates with two for statemantes through rows and columns,
+ * printing the numbers and verifying with if statements how many spaces after
+ * the comma.
+ * Return: Always 0.
+ */
 int timetable(void)
 {
 	    times_table();
