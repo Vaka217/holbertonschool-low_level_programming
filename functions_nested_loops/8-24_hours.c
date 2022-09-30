1deac9e0e4fb1a8311f0f020c0dcea1a5e560e60
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-09-29 17:27:13 -0700

    adds prototype function to main.h and C program 8-24_hours

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "_putchar.c"
+#include "_24_hours.c"
+
+/**
+ * hrs - prints every minute of the day starting from 00:00 to 23:59.
+ *
+ * Description: Prints itereting with a for the hours and with another for the
+ * minutes.
+ * Return: Always 0.
+ */
+int hrs(void)
+{
+	jack_bauer();
+	return (0);
+}
