086b6106d17ac99866a60b55296845a901231756
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-03 08:06:47 -0700

    fix 0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
   * _isupper - Checks for uppercase character
-  * @parameterc: an integer that holds the ASCII value of a character.
+  * @c: an integer that holds the ASCII value of a character.
   *
   * Description: Checks if character is uppercase looking its ASCII value.
   * Return: 1 if uppercase character, 0 otherwise.
   */
 int _isupper(int c)
 {
-	if ((65 <= c) && (c <= 90))
+	if ((c >= 68) && (c <= 90))
 		return (1);
 	else
 		return (0);
