383e6b87ab07835c37bbe3b498230743f46297a9
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-03 09:45:20 -0700

    Task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * @n - for counter
+ *
+ * Description: putchar prints the value of the char variable n, n initialize
+ * with '0' value and the for statement iterates until n ends with '9' value
+ * and uses putchar with each value of n.
+ * Return: void.
+ */
+void print_numbers(void)
+{
+	char n;
+
+	for (n = '0' ; n <= '9' ; n++)
+		_putchar(n);
+	_putchar('\n');
+	return;
+}
