fdfd32a9aba44739b476343da71c06cf2e1f7506
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-03 08:31:01 -0700

    Task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * mul - multiplies two integers
+  * @a: enter integer value
+  * @b: enter integer value
+  *
+  * Description: Multiplies two integers a and b.
+  * Return: Result of a * b.
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,6 +2,8 @@
 #define main_h
 
 #include <stdio.h>
+#include "2-mul.c"
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
