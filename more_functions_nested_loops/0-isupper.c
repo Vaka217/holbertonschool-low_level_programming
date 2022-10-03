a95ceb8a05cecb8f49c6bc44a81a7ebcadaae3d3
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-03 07:51:00 -0700

    fix 3 tASK 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 /**
-  * isupper - Checks for uppercase character
+  * _isupper - Checks for uppercase character
   *
   * Description: Checks if character is uppercase looking its ASCII value.
   * Return: 1 if uppercase character, 0 otherwise.
   */
-int isupper(int c)
+int _isupper(int c)
 {
 	if ((65 <= c) && (c <= 90))
 		return (1);
