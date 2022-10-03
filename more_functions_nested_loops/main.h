5831ac4f39b58d58b4b3cd3f01216d47c90a5d41
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-03 09:57:15 -0700

    Task 4

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,9 +2,9 @@
 #define main_h
 
 #include <stdio.h>
-int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif
