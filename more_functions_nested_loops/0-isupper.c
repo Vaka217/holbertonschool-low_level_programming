ca4a3ee1d30e4708f77e2983ff6a08936c026984
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-03 07:15:52 -0700

    adds 0.isupper.c and main.h

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+/**
+  * isupper - Checks for uppercase character
+  *
+  * Description: Checks if character is uppercase looking its ASCII value.
+  * Return: 1 if uppercase character, 0 otherwise.
+  */
+int isupper(int c)
+{
+	if ((65 <= c) && (c <= 90))
+		return (1);
+	else
+		return (0);
+}
