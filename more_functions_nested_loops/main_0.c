4ece4d6c7f5f48fd1622294ec5d9180d7e4565fc
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-03 07:47:01 -0700

    fix Task 0

---
+++ more_functions_nested_loops/main_0.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include "0-isupper.c"
+/**
+ * main - Checks for uppercase letter
+ *
+ * Description: Prints the character and 1 if its uppercase and
+ * 0 otherwise.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
