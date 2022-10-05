58086783f1431089e65f1e4fff775557810b29a4
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-05 08:05:17 -0700

    Task 16

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define LONGEST 10000000000
+  /** main - prints the 50 first Fibonacci numbers.
+  *
+  * Description: Prints the 50 first Fibonacci numbers. t1 and t2 contains
+  * the two previous numbers and nextt is the current number to print.
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	long int i, t1, p1, p2 = 0;
+	long int t2 = 1;
+	long int nextt = t1 + t2;
+
+	for (i = 1; i <= 92; i++)
+	{
+		printf("%ld, ", nextt);
+		t1 = t2;
+		t2 = nextt;
+		if (t1 + t2 > LONGEST)
+		{
+			p1 = (t1 + t2) / LONGEST;
+			p2 = (t1 + t2) % LONGEST;
+			printf("%ld%ld", p1, p2);
+		}
+		nextt = t1 + t2;
+	}
+	printf("%ld\n", nextt);
+	return (0);
+}
+
