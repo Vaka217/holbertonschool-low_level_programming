c9ec511963db36d226f956dc6beea762d8457896
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-06 10:16:28 -0700

    Betty fix Task 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
 /**
+  * _strlen - returns the length of a string
+  * @s: string parameter
   *
+  * Description: Counts each byte of the string, and that counts the length
+  * of the string.
+  * Return: Length of a string.
   */
 
 int _strlen(char *s)
