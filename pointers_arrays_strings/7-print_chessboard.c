b1f1b3691e8812ed7413d0a75828ff03c185d86e
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-12 10:54:35 -0700

    Task 6

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
+  * print_chessboard - prints the chessboard.
+  * @a: Array of chessboard.
   *
+  * Description: prints the chessboard.
+  * Return: void.
   */
 
 void print_chessboard(char (*a)[8])
 {
 	int i = 0, j = 0;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
