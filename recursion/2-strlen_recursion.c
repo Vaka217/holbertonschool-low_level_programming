3b4e3a934ddd9707ca27c5957a61f798ae290804
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-14 08:09:49 -0700

    Task 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _print_rev_recursion - prints a string in reverse..
+  * @s: String to print.
+  */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return 0;
+}
