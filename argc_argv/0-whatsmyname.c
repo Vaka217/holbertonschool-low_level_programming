8a2c6aac1759d661a458966a46e07be7b50527ae
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-17 07:02:18 -0700

    Task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
 /**
+  * main - prints its name, followed by a new line.
+  * @argc: Count number of arguments.
+  * @argv: Array of strings (arguments).
   *
+  * Return: 0.
   */
 
 int main(int argc, char **argv)
