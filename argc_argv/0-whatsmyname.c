e310d75239a31fb11c4985c39dfe3e2219160c63
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-17 06:51:41 -0700

    Task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+  *
+  */
+
+void whatsmyname(int argc, char **argv)
+	printf("%s\n", argv[0]);
---
+++ argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+void whatsmyname(int argc, char **argv);
+#endif
