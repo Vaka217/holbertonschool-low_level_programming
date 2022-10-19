29e26ccf960486250372f058951691d29e16e226
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-19 07:57:14 -0700

    Task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -9,12 +9,12 @@
 char *_strdup(char *str)
 {
 	char *dup = NULL;
-	unsigned long int i = 0, j = strlen(str) + 1;
+	unsigned long int i = 0;
 
 	if (str == NULL)
 		return (NULL);
-	dup = malloc(sizeof(char) * j);
-	for (i = 0; i <= j; i++)
+	dup = malloc(sizeof(char) * strlen(str + 1));
+	for (i = 0; i <= strlen(str + 1); i++)
 		dup[i] = str[i];
 	if (dup == NULL)
 		return (NULL);
