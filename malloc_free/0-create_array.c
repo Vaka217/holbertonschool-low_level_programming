43570c982d32675ccbd5ae630fbfc9610c640eb2
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-19 07:09:22 -0700

    fix Task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_array(unsigned int size, char c)
 	char *str = NULL;
 	unsigned int i = 0;
 
-	if (size != 0)
+	str = malloc(sizeof(*str) * size);
+	if (str != 0)
 	{
-		str = malloc(sizeof(*str) * size);
 		for (i = 0; i <= size; i++)
 			str[i] = c;
 		return (str);
 	}
-	return (str);	
+	return (str);
 }
