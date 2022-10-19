f8f74bf8ad091cb10c2185a9611e9e69c8126818
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-19 13:52:23 -0700

    Task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -19,10 +19,13 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
-		if (grid[i] == NULL)
+		if (!grid[i])
 		{
 			for (j = 0; j < height; j++)
-				free(grid[i]);
+			{
+				if (grid[i])
+					free(grid[i]);
+			}
 			free(grid);
 			return (NULL);
 		}
