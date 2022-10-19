1217cfdfa675982aa4f1f82aeeef044e87fd60dd
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-19 12:27:46 -0700

    Task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 /**
   *
@@ -9,18 +8,27 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3 = NULL;
-	size_t i, j, w = 0;
+	int i = 0, j = 0, len = 0, len2 = 0;
 
-	s3 = malloc(strlen(s1) + strlen(s2));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (len = 0; s1[len] != '\0'; len++)
+	{
+	}
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{
+	}
+	s3 = malloc(len + len2 + 1);
 	if (s3 == NULL)
 		return (NULL);
-	for (i = 0; i < strlen(s1) - 1; i++)
+	for (i = 0; i < len; i++)
 		s3[i] = s1[i];
-	for (j = i; j < strlen(s1) + strlen(s2); j++)
+	for (i = len; i < len + len2 + 1; i++)
 	{
-		s3[j] = s2[w];
-		w++;
+		s3[i] = s2[j];
+		j++;
 	}
-	/*s3[j + 1] = '\0';*/
 	return (s3);
 }
