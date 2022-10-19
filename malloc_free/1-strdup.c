0cb7887d88cbc6f29d65c121986baf270d5f89a3
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-19 08:00:24 -0700

    Task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
   *
@@ -9,12 +8,16 @@
 char *_strdup(char *str)
 {
 	char *dup = NULL;
-	unsigned long int i = 0;
+	int i = 0, j = 0;
 
 	if (str == NULL)
 		return (NULL);
-	dup = malloc(sizeof(char) * strlen(str + 1));
-	for (i = 0; i <= strlen(str + 1); i++)
+	for (j = 0; j <= *str; j++)
+	{
+	}
+	j++;
+	dup = malloc(sizeof(char) * j);
+	for (i = 0; i < j; i++)
 		dup[i] = str[i];
 	if (dup == NULL)
 		return (NULL);
