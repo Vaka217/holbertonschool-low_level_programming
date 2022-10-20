a7ca4152a2dc4667983e308e992d639b0825c017
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-20 08:15:33 -0700

    Task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  *
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *c = NULL;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	c = malloc(size * nmemb);
+	if (!c)
+		return (NULL);
+	return (c);
+}
