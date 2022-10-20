104147d9b88f4425f09122a96883cd0d101a8f91
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-20 08:26:03 -0700

    fix Task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -8,12 +8,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *c = NULL;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	c = malloc(size * nmemb);
 	if (!c)
 		return (NULL);
-	*c = c[0];
+	for (i = 0; i < nmemb; i++)
+		c[i] = 0;
 	return (c);
 }
