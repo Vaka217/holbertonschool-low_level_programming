034624cb815eaefecf6d4a0dd40e0b7ec9be5797
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-20 06:48:08 -0700

    Task 0 fix betty

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 
 /**
+  * malloc_checked - allocates memory using malloc.
+  * @b: Size bytes for malloc.
   *
+  * Return: A pointer to the allocated memory or cause normal process
+  * termination with a status value of 98 if malloc fails.
   */
 
 void *malloc_checked(unsigned int b)
