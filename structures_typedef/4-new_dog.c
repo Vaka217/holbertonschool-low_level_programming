daa41af0a85745d46dab653c4915b48b6b54cb23
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-27 10:15:04 -0700

    Task 4 fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,8 +1,7 @@
 #include "dog.h"
 #include <stddef.h>
 #include <stdlib.h>
-#include "2-strlen.c"
-#include "9-strcpy.c"
+#include "1-strdup.c"
 
 /**
   *
@@ -11,30 +10,25 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dogcp;
-	int ownerlen = 0, namelen = 0;
 
 	if (name != NULL && owner != NULL)
 	{
-		namelen = _strlen(name) + 1;
-		ownerlen = _strlen(owner) + 1;
 		dogcp = malloc(sizeof(dog_t));
 		if (dogcp == NULL)
 			return (NULL);
-		dogcp->name = malloc(sizeof(char) * namelen);
+		dogcp->name = _strdup(name);
 		if (dogcp->name == NULL)
 		{
 			free(dogcp);
 			return (NULL);
 		}
-		dogcp->owner = malloc(sizeof(char) * ownerlen);
+		dogcp->owner = _strdup(owner);
 		if (dogcp->owner == NULL)
 		{
 			free(dogcp->name);
 			free(dogcp);
 			return (NULL);
 		}
-		dogcp->name = _strcpy(dogcp->name, name);
-		dogcp->owner = _strcpy(dogcp->owner, owner);
 		dogcp->age = age;
 	}
 	return (dogcp);
