5a923652b0c1a86cfeded851fc9f5a4cfd82684c
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-27 10:29:16 -0700

    Task 5

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 
 /**
+  * free_dog - frees dogs.
+  * @d: The dog.
   *
   */
 
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
