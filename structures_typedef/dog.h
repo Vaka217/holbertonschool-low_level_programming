e462726f5032c45e5d4910d57b580d9b5ee14a19
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-27 06:03:01 -0700

    Task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+  * struct dog - Description of a dog.
+  * @name: Name of a dog.
+  * @owner: Owner of a dog.
+  * @age: Age of a dog.
+  *
+  * Description: Name, owner and age of a dog.
+  */
+
+struct dog
+{
+	char *name, *owner;
+	float age;
+};
+
+#endif
