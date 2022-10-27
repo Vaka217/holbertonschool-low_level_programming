bd8e93e90d83703d691b51ecafebe43dba2abad8
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-27 08:09:17 -0700

    Task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,8 +10,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dogcp;
 	dogcp = malloc(sizeof(dog_t));
 
-	dogcp->name = name;
-	dogcp->age = age;
-	dogcp->owner = owner;
+	if (dogcp == NULL)
+		return (NULL);
+	if (name == NULL)
+		return (NULL);
+	else
+		dogcp->name = name;
+	if (age == 0)
+	{
+		free(dogcp);
+		return (NULL);
+	}
+	else
+		dogcp->age = age;
+	if (owner == NULL)
+	{
+		free(dogcp);
+		return (NULL);
+	}
+	else
+		dogcp->owner = owner;
+
 	return (dogcp);
 }
