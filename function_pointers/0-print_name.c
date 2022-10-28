3d29f7548ebfe203c82a7bd7f531792aa22e70b9
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-10-28 06:13:16 -0700

    Task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+  * print_name - prints a name.
+  * @name: String which is the name.
+  * @f: Function pointer to uppercase name function or as passed name.
+  *
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
