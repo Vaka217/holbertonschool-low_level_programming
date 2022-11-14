1a0aa1792cbd29c075ce9a46abb0be3f5fb2f256
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-14 06:59:37 -0800

    Fix task 2 return value

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -8,12 +8,10 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 
+	if (!new)
+		return (NULL);
 	new->str = strdup(str);
 	new->next = *head;
 	*head = new;
-	free(new);
-	if (*head)
-		return (*head);
-	else
-		return (NULL);
+	return (new);
 }
