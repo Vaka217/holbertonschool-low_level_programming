84e6e9aa2cea6e21da6ff25d2ba5729a6d4ca926
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-14 07:11:41 -0800

    fix Task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,11 +11,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
+	char *strcp;
 
-	if (!new || !str)
+	if (!new)
 		return (NULL);
-	new->str = strdup(str);
-	new->len = strlen(new->str);
+	strcp = strdup(str);
+	if (!strcp)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->str = strcp;
+	new->len = strlen(strcp);
 	new->next = *head;
 	*head = new;
 	return (new);
