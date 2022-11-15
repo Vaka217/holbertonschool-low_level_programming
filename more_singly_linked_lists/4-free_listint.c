f8e11a41e59185310be62cb140e0ce8cd9f8169c
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-15 05:38:31 -0800

    Task 5 and fix Task 4 betty documentation error

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 
 /**
+  * free_listint - frees a listint_t list.
+  * @head: The head node of the list.
   *
   */
 
---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include "4-free_listint.c"
+/**
+  * free_listint2 - frees a listint_t list.
+  * @head: Pointer to the head node of the list.
+  *
+  */
+
+void free_listint2(listint_t **head)
+{
+	free_listint(*head);
+	*head = NULL;
+}
