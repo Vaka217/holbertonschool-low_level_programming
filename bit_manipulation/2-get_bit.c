9108be4e2c87a70d3e79cd730fa8ab0ba1f068f6
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-16 09:16:02 -0800

    Task 2 final fix

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
-  * print_binary - prints the binary representation of a number.
-  * @n: Number to print in binary.
+  * get_bit - returns the value of a bit at a given index.
+  * @n: Number to find the bit.
+  * @index: Index where the bit is.
   *
+  * Return: the value of the bit at the given index or -1 if an error occured.
   */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-
-	if (0x01 << index == 0)
+	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 	return ((n >> index) & 1);
 }
