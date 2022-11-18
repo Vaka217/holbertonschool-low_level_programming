11db96c368e3961aea04b05cd0b6c48c828439a1
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-18 05:56:48 -0800

    Task 6 fix betty

--- bit_manipulation/100-get_endianness.c
+++ bit_manipulation/100-get_endianness.c
@@ -1,5 +1,10 @@
-#include <stdint.h>
-#include <stdio.h>
+#include "main.h"
+
+/** get_endianness - checks the endianness.
+  *
+  * Return: 0 if big endian, 1 if little endian.
+  */
+
 int get_endianness(void)
 {
 	int i = 1;
