251b843917f8632355d1cd3af95ec12b093f2e08
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-21 08:41:07 -0800

    Fixed error when the file already exists in task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL, 0600);
 	if (errno == EEXIST)
 	{
 		close(fd);
