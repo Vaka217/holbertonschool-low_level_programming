08daaa97a0599cb8993ecdda830fda13c66dc59f
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-21 08:30:04 -0800

    Task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,24 +1,29 @@
 #include "main.h"
 
 /**
-  * read_textfile -  reads a text file and prints it to the POSIX stdout.
-  * @filename: Name of the file to read and print.
-  * @letters: Number of letters it should read and print.
+  * create_file - creates a file.
+  * @filename: Name of the file to create.
+  * @text_contet: NULL terminated string to write to the file.
   *
-  * Return: The number of letters it could read and print or 0 if the file
-  * cannot be opened or read, if filename is NULL, or if write fails or does
-  * not write the expected amount of bytes.
+  * Return: 1 on success, -1 on failure (file cannot be created, file cannot
+  * be written, write "fails", etc...).
   */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, wr;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (errno == EEXIST)
+		fd = open(filename, O_WRONLY | O_TRUNC);
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, strlen(text_content));
+	if (!text_content)
+		return (1);
+	wr = write(fd, text_content, strlen(text_content));
+	if (wr == -1)
+		return (-1);
 	return (1);
 }
