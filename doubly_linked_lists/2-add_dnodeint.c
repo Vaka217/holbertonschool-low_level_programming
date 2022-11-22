992a665c187b505ba59f475a6da09e48e53a0bd9
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-22 09:03:56 -0800

    Task 2 fix

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,6 +15,13 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (!(*head))
+	{
+		*head = new;
+		return (new);
+	}
 	new->next = *head;
 	(*head)->prev = new;
 	*head = new;
