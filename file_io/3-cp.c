83f776dc136f8d1a4d525330d35b75ce0a62cfe9
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-22 08:08:08 -0800

    Task 3 fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,11 +17,11 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s", av[1]), exit(98);
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
 	if (fd2 == -1)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s", av[2]);
+		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]);
 		close(fd), exit(99);
 	}
 	while (rd == 1024)
@@ -29,20 +29,20 @@ int main(int ac, char **av)
 		rd = read(fd, buf, 1024);
 		if (rd == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't read from file %s", av[1]);
+			dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		wr = write(fd2, buf, rd);
 		if (wr == -1)
 		{
-			dprintf(STDOUT_FILENO, "Error: Can't write to %s", av[2]);
+			dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 
 	}
 	if (close(fd) == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d", fd), exit(100);
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
 	if (close(fd2) == -1)
-		dprintf(STDOUT_FILENO, "Error: Can't close fd %d", fd), exit(100);
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
 	return (0);
 }
