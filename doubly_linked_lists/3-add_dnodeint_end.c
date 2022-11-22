5c903d97019492ee2d82cf03f3cd364218fc48fb
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-22 09:09:54 -0800

    Task 3 fix betty

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * add_nodeint_end - adds a new node at the end of a listint_t list.
+  * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
   * @head: Pointer to the head node of the list.
   * @n: Integer for the new node n.
   *
