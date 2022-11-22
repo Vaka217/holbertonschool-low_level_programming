2f21548c150f4a9b7ca6e754074d2298018199a7
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-22 08:16:42 -0800

    Task 3 fix file permissions

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,7 +18,8 @@ int main(int ac, char **av)
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
+	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP |
+			S_IWGRP | S_IROTH);
 	if (fd2 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
