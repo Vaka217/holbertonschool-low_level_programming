1b50e6fbedfacbe9210a481b437034342d9555a2
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-22 09:13:05 -0800

    Task 4 fix names

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -6,11 +6,11 @@
   *
   */
 
-void free_dlistint(listint_t *head)
+void free_dlistint(dlistint_t *head)
 {
 	if (head)
 	{
-		free_listint(head->next);
+		free_dlistint(head->next);
 		free(head);
 	}
 }
