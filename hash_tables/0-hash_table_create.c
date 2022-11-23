25f14c86cd30621b7fa15c32e6b36d20500fe38e
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-23 04:57:55 -0800

    Task 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+  *
+  */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	hash_node_t **array;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	array = malloc(size);
+	if (array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->array = array;
+	return (table);
+}
