9f04399dd955e9f3059528b5a057a50e71545d7a
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-23 07:57:13 -0800

    Task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,18 +1,40 @@
 #include "hash_tables.h"
 #include <string.h>
 /**
+  * hash_table_set - adds an element to the hash table.
+  * *@ht: Hash table.
+  * @key: The key.
+  * @value: Value of the key.
   *
+  * Return: 1 if it succeeded, 0 otherwise.
   */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{ 
+{
+	hash_node_t *tmp = malloc(sizeof(hash_node_t));
 	hash_node_t *new = malloc(sizeof(hash_node_t));
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
 
+	if (!ht)
+	{
+		free(tmp);
+		free(new);
+		return (0);
+	}
+	index = key_index((const unsigned char *)key, ht->size);
 	new->key = malloc(strlen(key) + 1);
 	new->value = malloc(strlen(value) + 1);
 	new->key = strdup(key);
 	new->value = strdup(value);
-	ht->array[index] = new;
+	if (ht->array[index] != NULL)
+	{
+		tmp = ht->array[index];
+		ht->array[index] = new;
+		ht->array[index]->next = tmp;
+	}
+	else
+		ht->array[index] = new;
+	free(tmp);
+	free(new);
 	return (1);
 }
