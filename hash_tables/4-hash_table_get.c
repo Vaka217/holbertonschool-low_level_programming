f84ef5daba0d91d1a2cd984073da8817819b7e51
Vaka217 <gonzalocabaleiro12@gmail.com>
2022-11-24 05:18:26 -0800

    Fix collisions task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -11,9 +11,16 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	long unsigned int index;
 
-	if (!ht->array[index])
+	if (!ht)
 		return (NULL);
-	return (ht->array[index]->value);
+	index = key_index((const unsigned char *)key, ht->size);
+	while (ht->array[index])
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
+	}
+	return (NULL);
 }
